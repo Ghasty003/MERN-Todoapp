e4041e1863f7506f3c8884f85eaef60bb3eb81f8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-30 13:34:47 -0400

    Initialized todos component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Input from '../components/Input';
+import Todos from '../components/Todos';
 
 function Home() {
     return (
         <div className='home'>
             <h2>Todo app</h2>
             <Input />
+            <Todos />
         </div>
     );
 }
