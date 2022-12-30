e071677fc86f22c622a52f569e9311321893d309
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-30 18:08:43 -0400

    Added edit todo functionality

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,16 +1,20 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdOutlineDownloadDone } from "react-icons/md";
 import TodosContext from '../context/TodosContext';
 
-function Todos({ todo }) {
+function Todos({ todo:t }) {
 
+    const [edit, setEdit] = useState(false);
+    const [todo, setTodo] = useState("");
+
+    const input = useRef("");
     const p = useRef("");
 
     const { dispatch } = useContext(TodosContext);
 
     const deleteTodo = async () => {
-        const response = await fetch("http://localhost:4000/api/todos/"+ todo._id, {
+        const response = await fetch("http://localhost:4000/api/todos/"+ t._id, {
             method: "DELETE",
         });
 
@@ -29,17 +33,62 @@ function Todos({ todo }) {
         p.current.classList.toggle("line")
     }
 
+    const editTodo = async () => {
+       const response = await fetch("http://localhost:4000/api/todos/"+ t._id);
+       const json = await response.json();
+
+       if (!response.ok) {
+        console.log(json.error)
+       }
+
+       if (response.ok) {
+            setEdit(true);
+            setTodo(json.todo);
+        }
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:4000/api/todos/" + t._id, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({todo})
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            console.log(json.error);
+        }
+
+        if (response.ok) {
+            setEdit(false);
+            console.log(json);
+        }
+    }
+
     return (
-        <div className='todo-container'>
-            <div className='todos'>
-                <p ref={p}>{todo.todo}</p>
-                <div>
-                    <AiFillEdit size={25} cursor="pointer" title='Edit' />
-                    <AiFillDelete onClick={deleteTodo} size={25} cursor="pointer" title='Delete' />
-                    <MdOutlineDownloadDone onClick={handleDone} size={25} cursor="pointer" title='Done' />
+        <div className='parent-todo'>
+            { edit ? <form onSubmit={handleSubmit}>
+                        <input ref={input} type="text" value={todo} onChange={e => setTodo(e.target.value)} />
+                        <button>Done</button>
+                    </form>
+            
+            : <div className='todo-container'>
+                <div className='todos'>
+                    <p ref={p}>{t.todo}</p>
+                    <div>
+                        <AiFillEdit size={25} cursor="pointer" title='Edit' onClick={editTodo} />
+                        <AiFillDelete onClick={deleteTodo} size={25} cursor="pointer" title='Delete' />
+                        <MdOutlineDownloadDone onClick={handleDone} size={25} cursor="pointer" title='Done' />
+                    </div>
                 </div>
-            </div>
-            <span>{todo.createdAt}</span>
+                <span>{t.createdAt}</span>
+            </div> }
         </div>
     );
 }
