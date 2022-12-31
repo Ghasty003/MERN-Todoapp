ed77cf8ddad49268ec2e15cd0fd205d5415d7286
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-31 15:14:59 -0400

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/components/Todos.jsx",
     },
   },
   plugins: [react()],
