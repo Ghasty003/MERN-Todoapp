a59bca858061a82c5da575a63aabaf2a083f28cb
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-01 10:41:45 -0400

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/components/Todos.jsx",
-    },
-  },
   plugins: [react()],
 });
